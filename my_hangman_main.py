2e44910c63c839a74fa8eb22f117c3dc86750c52
EthanGReinhart <155490515+EthanGReinhart@users.noreply.github.com>
2024-01-16 15:30:17 -0500

    Update my_hangman_main.py

--- my_hangman_main.py
+++ my_hangman_main.py
@@ -4,28 +4,33 @@ import random
 words_list = ['apple', 'banana', 'orange', 'pickle', 'rocket', 'turtle', 'python', 'guitar', 'jacket', 'window',
               'camera', 'purple', 'rabbit', 'pencil', 'summer', 'pocket', 'sunset', 'soccer', 'planet', 'purple']
 
+
 def get_word():
     word = random.choice(words_list)
     return [*word]
 
-def print_word(word):
-    word_as_list = ['_'] * len(word)
-    for i in word_as_list:
+
+word = get_word()
+progress = ['_'] * len(word)
+
+
+
+def print_progress(progress):
+    for i in progress:
         print(i + ' ', end="")
 
 
-def guess_letter(word):
+def guess_letter(word, progress):
     guessed_letters = []
     guess = input("What letter would you like to guess? ")
-    word_as_list = ['_'] * len(word)
     if guess.isalpha():
         if guess in word:
             guessed_letters.append(guess)
             print(guessed_letters)
-            for i, letter in enumerate(words_list):
+            for i, letter in enumerate(word):
                 if guess == letter:
-                    word_as_list[i] = guess.upper()
-            return word
+                    progress[i] = guess.upper()
+            return progress
         else:
             print("Letter not in word")
             guessed_letters.append(guess)
@@ -38,9 +43,10 @@ run = True
 while run:
     will_play = input("\nWould you like to play? (Y/N) ")
     if will_play.lower() == "y":
-        word = get_word()
-        guess_letter(word, words_list)
-        print(word)
-        print_word(word)
+        round_loop = True
+        while round_loop:
+            progress = guess_letter(word, progress)
+            print(word)
+            print_progress(progress)
 
 
